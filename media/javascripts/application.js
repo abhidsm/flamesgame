a41c4064146c791d13173fa0184892d7d63b89c3
Abhilash M A <abhidsm@gmail.com>
2011-02-20 18:55:25 +0530

    add ajax to show result

---
+++ media/javascripts/application.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$("#flames-form").submit(function(){
+		$.post($("#flames-form").attr('action'),
+		       $("#flames-form").serialize(),
+		       function(data){
+			   response = JSON.parse(data);
+			   $('#result h3').html(response.flames.result);
+			   resetResultClass();
+			   $('#result').addClass(response.flames.status);
+		       }	      
+		       );
+		return false;
+	    });
+    });
+
+function resetResultClass(){
+    $('#result').removeClass();
+    $('#result').addClass('center');
+    $('#result').addClass('span-11');
+    $('#result').addClass('last');
+}
