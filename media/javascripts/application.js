a8cab82d1828ce7d65ded9ca8ffa18cedfdf4087
abhilash <abhidsm@gmail.com>
2011-04-22 15:49:29 +0530

    Added form validations

--- media/javascripts/application.js
+++ media/javascripts/application.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
-	$("#flames-form").submit(function(){
-		$.post($("#flames-form").attr('action'),
-		       $("#flames-form").serialize(),
-		       function(data){
-			   response = JSON.parse(data);
-			   $('#result h3').html(response.flames.result);
-			   resetResultClass();
-			   $('#result').addClass(response.flames.status);
-		       }	      
-		       );
-		return false;
-	    });
+	bindFormValidation();
     });
+
+function formSubmitAction() {
+    $.post($("#flames-form").attr('action'),
+	   $("#flames-form").serialize(),
+	   function(data){
+	       response = JSON.parse(data);
+	       $('#result h3').html(response.flames.result);
+	       resetResultClass();
+	       $('#result').addClass(response.flames.status);
+	   }	      
+	   );
+    return false;
+}
 
 function resetResultClass(){
     $('#result').removeClass();
@@ -20,3 +22,21 @@ function resetResultClass(){
     $('#result').addClass('last');
     $('#result').show();
 }
+
+function bindFormValidation() {
+    $('#flames-form').validate({
+	    rules: {
+		your_name: "required",
+		partner_name: "required"
+		    },
+	    messages: {
+		your_name: "Please enter your name",
+		partner_name: "Please enter your partner name"
+		    },
+		focusCleanup: true,
+		submitHandler: function(form) {
+		formSubmitAction();
+	    }
+	});
+}
+ 
